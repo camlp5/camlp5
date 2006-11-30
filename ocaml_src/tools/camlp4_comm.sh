cf357f9e154a1c9ecc63f9552542f28faff8277e
deraugla <deraugla>
2006-11-30 13:36:08 +0000

    -

--- ocaml_src/tools/camlp4_comm.sh
+++ ocaml_src/tools/camlp4_comm.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-if test "$(basename $1 .mli).mli" = "$1"; then
-  echo cp $1 $(basename $1 .mli).ppi
-  cp $1 $(basename $1 .mli).ppi
+FILE=
+while test "" != "$1"; do
+	case "$1" in
+	*.ml*) FILE=$1; break;;
+	esac
+	shift
+done
+
+if test "$(basename $FILE .mli).mli" = "$FILE"; then
+  echo cp $FILE $(basename $FILE .mli).ppi
+  cp $FILE $(basename $FILE .mli).ppi
 else
-  echo cp $1 $(basename $1 .ml).ppo
-  cp $1 $(basename $1 .ml).ppo
+  echo cp $FILE $(basename $FILE .ml).ppo
+  cp $FILE $(basename $FILE .ml).ppo
 fi
