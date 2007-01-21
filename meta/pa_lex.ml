df448cfeccd70164d8c24923c834de1ac35a544e
deraugla <deraugla>
2007-01-21 19:32:55 +0000

    -

--- meta/pa_lex.ml
+++ meta/pa_lex.ml
@@ -16,15 +16,16 @@ value add_char loc c cl = <:expr< [$c$ :: $cl$] >>;
 value get_buf loc cl = cl;
 *)
 
+value accum_chars loc cl =
+  List.fold_right (add_char loc) cl <:expr< $lid:var$ >>
+;
+
 value mk_parser loc rl =
   let rl =
     List.map
       (fun (sl, cl, a) ->
          let a =
-           let b =
-             let b = <:expr< $lid:var$ >> in
-             if cl = [] then b else List.fold_right (add_char loc) cl b
-           in
+           let b = accum_chars loc cl in
            match a with
            [ Some e -> <:expr< ($e$, $get_buf loc b$) >>
            | None -> b ]
@@ -38,7 +39,8 @@ value mk_parser loc rl =
 EXTEND
   GLOBAL: expr;
   expr:
-    [ [ "lexer"; rl = rules -> mk_parser loc rl ] ]
+    [ [ "lexer"; rl = rules ->
+          <:expr< fun $lid:var$ -> $mk_parser loc rl$ >> ] ]
   ;
   rules:
     [ [ "["; rl = LIST0 rule SEP "|"; "]" -> rl ] ]
@@ -61,9 +63,7 @@ EXTEND
           ([s :: sl], [<:expr< c >> :: cl])
       | (sl, cl) = symbs; i = LIDENT; errk = err_kont ->
           let s =
-            let buf =
-              List.fold_right (add_char loc) cl <:expr< $lid:var$ >>
-            in
+            let buf = accum_chars loc cl in
             let e = <:expr< $lid:i$ $buf$ >> in
             (Exparser.SpNtr loc <:patt< $lid:var$ >> e, errk)
           in
