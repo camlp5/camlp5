7a8e7cd4bf2c7461ff2ba5b7daafc5b992118044
deraugla <deraugla>
2007-06-03 20:04:03 +0000

    -

---
+++ etc/pr_rpnew.ml
@@ -0,0 +1,25 @@
+(* camlp4r q_MLast.cmo ./pa_extfun.cmo *)
+(* $Id$ *)
+(* Copyright (c) INRIA 2007 *)
+
+open Pcaml.NewPrinter;
+open Sformat;
+
+value not_impl name ind b x k =
+  let desc =
+    if Obj.tag (Obj.repr x) = Obj.tag (Obj.repr "") then
+      sprintf "\"%s\"" (Obj.magic x)
+    else if Obj.is_block (Obj.repr x) then
+      "tag = " ^ string_of_int (Obj.tag (Obj.repr x))
+    else "int_val = " ^ string_of_int (Obj.magic x)
+  in
+  sprintf "%s\"pr_rp, not impl: %s; %s\"%s" b name (String.escaped desc) k
+;
+
+let lev = find_pr_level "top" pr_expr.pr_levels in
+lev.pr_rules :=
+  extfun lev.pr_rules with
+  [ <:expr< fun (strm__ : Stream.t _) -> $e$ >> ->
+      fun curr next ind b k -> not_impl "parser" ind b e k ]
+;
+
