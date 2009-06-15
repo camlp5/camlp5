087798cf115c6d6b2173beeef57133047c9d78b8
deraugla <deraugla>
2009-06-15 14:59:53 +0000

    -

--- main/ast2pt.mli
+++ main/ast2pt.mli
@@ -1,12 +1,24 @@
 (* camlp5r *)
-(* $Id: ast2pt.mli,v 1.4 2007/08/23 08:32:35 deraugla Exp $ *)
+(* $Id: ast2pt.mli,v 1.5 2007/08/23 08:49:52 deraugla Exp $ *)
 (* Copyright (c) INRIA 2007 *)
 
+(** Conversion between Camlp5 AST into OCaml AST *)
+
 value interf : string -> list MLast.sig_item -> Parsetree.signature;
+   (** [interf fname ast] return the OCaml equivalent AST of an
+       interface (mli file), [fname] being the input file name. *)
 value implem : string -> list MLast.str_item -> Parsetree.structure;
+   (** [implem fname ast] return the OCaml equivalent AST of an
+       implementation (ml file), [fname] being the input file name. *)
 value phrase : MLast.str_item -> Parsetree.toplevel_phrase;
+   (** [phrase sil] return the OCaml equivalent AST of a toplevel phrase. *)
 
 value mkloc : Stdpp.location -> Location.t;
+   (** Convert a Camlp5 location into a OCaml location. *)
 
 value fast : ref bool;
+   (** Flag to generate fast (unsafe) access to arrays. Default: False. *)
 value no_constructors_arity : ref bool;
+   (** Flag to generate nodes telling that constructor arity is not taken
+       into account in the AST (e.g. True for normal syntax, False for
+       revised syntax). Default: False. *)
