bc56dd7d8afe6a311c84d52c732c739b565a719e
deraugla <deraugla>
2009-06-15 14:29:19 +0000

    -

--- camlp4/pcaml.ml
+++ camlp4/pcaml.ml
@@ -12,7 +12,7 @@
 
 (* $Id$ *)
 
-value version = "4.03";
+value version = "4.04-exp";
 value syntax_name = ref "";
 
 value gram =
--- meta/mk_q_MLast.sh
+++ meta/mk_q_MLast.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-# $Id: mk_q_MLast.sh,v 1.1 2006/09/29 04:45:49 deraugla Exp $
+# $Id: mk_q_MLast.sh,v 1.2 2007/07/11 03:13:56 deraugla Exp $
 
 IFILE=pa_r.ml
 OFILE=q_MLast.ml
 (
 sed -e '/^EXTEND$/,$d' $OFILE
 echo EXTEND
-ocamlrun ./camlp4r -I . -I ../etc q_MLast.cmo pa_extend.cmo pr_r.cmo pr_extend.cmo -cip -quotify $IFILE | sed -e '1,/^EXTEND$/d' -e '/^END;$/,$d'
+ocamlrun ./${NAME}r -I . -I ../etc q_MLast.cmo pa_extend.cmo pr_r.cmo pr_extend.cmo -cip -quotify $IFILE | sed -e '1,/^EXTEND$/d' -e '/^END;$/,$d'
 echo '  (* Antiquotations for local entries *)'
 sed -e '1,/Antiquotations for local entries/d' $OFILE
 )
