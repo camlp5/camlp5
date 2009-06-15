f28bf95fe24e80dc65c977c7f1501c53e8bd9a4b
deraugla <deraugla>
2009-06-15 23:55:33 +0000

    -

--- main/pcaml.ml
+++ main/pcaml.ml
@@ -1,8 +1,8 @@
 (* camlp5r pa_macro.cmo pa_extend.cmo *)
-(* $Id: pcaml.ml,v 1.47 2007/11/27 10:14:52 deraugla Exp $ *)
+(* $Id: pcaml.ml,v 1.48 2007/11/27 12:04:05 deraugla Exp $ *)
 (* Copyright (c) INRIA 2007 *)
 
-value version = "5.04";
+value version = "5.05-exp";
 value syntax_name = ref "";
 
 value gram =
