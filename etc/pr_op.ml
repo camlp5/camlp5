979e8c8d3484c456d26a6114b72b61f1f26da5e9
deraugla <deraugla>
2009-06-15 14:28:46 +0000

    -

--- etc/pr_op.ml
+++ etc/pr_op.ml
@@ -1,5 +1,5 @@
 (* camlp4r q_MLast.cmo ./pa_extfun.cmo *)
-(* $Id: pr_op.ml,v 1.4 2007/07/06 09:38:42 deraugla Exp $ *)
+(* $Id: pr_op.ml,v 1.5 2007/07/06 09:52:15 deraugla Exp $ *)
 (* Copyright (c) INRIA 2007 *)
 
 (* Heuristic to rebuild parsers and streams from the AST *)
@@ -481,14 +481,19 @@ value parser_body pc (po, spel) =
 value print_parser pc e =
   match e with
   [ <:expr< fun (strm__ : Stream.t _) -> $e$ >> ->
-      let pa = unparser_body e in
+      let (po, spel) = unparser_body e in
       let (op_begin, op_end, sh, pc_dang) =
-        if pc.dang = ";" then ("(", ")", 1, "") else ("", "", 0, pc.dang)
+        if List.mem pc.dang ["|"; ";"] then ("(", ")", 1, "")
+        else ("", "", 0, pc.dang)
+      in
+      let spel =
+        if spel = [] then [([], None, <:expr< raise Stream.Failure >>)]
+        else spel
       in
       parser_body
         {ind = pc.ind + sh; bef = sprintf "%s%sparser" pc.bef op_begin;
          aft = sprintf "%s%s" op_end pc.aft; dang = pc_dang}
-        pa
+        (po, spel)
   | e -> expr pc e ]
 ;
 
