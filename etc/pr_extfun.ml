1939d1cfe9de8a05ec25f75828dc120721a8d40a
deraugla <deraugla>
2009-06-16 03:10:46 +0000

    -

--- etc/pr_extfun.ml
+++ etc/pr_extfun.ml
@@ -1,5 +1,5 @@
 (* camlp5r q_MLast.cmo ./pa_extfun.cmo ./pa_extprint.cmo ./pa_pprintf.cmo *)
-(* $Id: pr_extfun.ml,v 1.17 2008/01/04 14:35:43 deraugla Exp $ *)
+(* $Id: pr_extfun.ml,v 1.18 2008/01/04 19:33:01 deraugla Exp $ *)
 (* Copyright (c) INRIA 2007-2008 *)
 
 (* heuristic to rebuild the extfun statement from the AST *)
@@ -63,62 +63,20 @@ value patt_as pc z =
 
 value match_assoc pc (p, w, e) =
   let pc_dang = if pc.aft = "" then "|" else "" in
-  horiz_vertic
-    (fun () ->
-       sprintf "%s%s%s -> %s%s" pc.bef
-         (patt_as {(pc) with bef = ""; aft = ""} p)
-         (match w with
-          [ <:vala< Some e >> ->
-              sprintf " when %s" (expr {(pc) with bef = ""; aft = ""} e)
-          | _ -> "" ])
-         (comm_expr expr {(pc) with bef = ""; aft = ""; dang = pc_dang} e)
-         pc.aft)
-    (fun () ->
-       let patt_arrow k =
-         match w with
-         [ <:vala< Some e >> ->
-             horiz_vertic
-               (fun () ->
-                  sprintf "%s%s when %s ->%s" pc.bef
-                    (patt_as {(pc) with bef = ""; aft = ""} p)
-                    (expr {(pc) with bef = ""; aft = ""} e) k)
-               (fun () ->
-                  let s1 = patt_as {(pc) with aft = ""} p in
-                  let s2 =
-                    horiz_vertic
-                      (fun () ->
-                         sprintf "%swhen %s ->%s" (tab pc.ind)
-                           (expr {(pc) with bef = ""; aft = ""} e) k)
-                      (fun () ->
-                         let s1 = sprintf "%swhen" (tab pc.ind) in
-                         let s2 =
-                           expr
-                             {(pc) with ind = pc.ind + 2;
-                              bef = tab (pc.ind + 2); aft = sprintf " ->%s" k}
-                             e
-                         in
-                         sprintf "%s\n%s" s1 s2)
-                  in
-                  sprintf "%s\n%s" s1 s2)
-         | _ -> patt_as {(pc) with aft = sprintf " ->%s" k} p ]
-       in
-       let s1 = patt_arrow "" in
-       let s2 =
-         comm_expr expr
-           {(pc) with ind = pc.ind + 2; bef = tab (pc.ind + 2);
-            dang = pc_dang}
-           e
-       in
-       sprintf "%s\n%s" s1 s2)
+  match w with
+  [ <:vala< Some e1 >> ->
+      pprintf pc "%p@ @[when@;%p ->@]@;%q" patt_as p expr e1
+        (comm_expr expr) e pc_dang
+  | _ ->
+      pprintf pc "%p ->@;%q" patt_as p (comm_expr expr) e pc_dang ]
 ;
 
 value match_assoc_sh pc pwe = match_assoc {(pc) with ind = pc.ind + 2} pwe;
 
 value match_assoc_list pc pwel =
-  if pwel = [] then sprintf "%s[]%s" pc.bef pc.aft
+  if pwel = [] then pprintf pc "[]"
   else
-    vlist2 match_assoc_sh (bar_before match_assoc_sh)
-      {(pc) with bef = sprintf "%s[ " pc.bef; aft = sprintf " ]%s" pc.aft}
+    pprintf pc "[ %p ]" (vlist2 match_assoc_sh (bar_before match_assoc_sh))
       pwel
 ;
 
@@ -127,12 +85,7 @@ EXTEND_PRINTER
     [ [ <:expr< Extfun.extend $e$ $list$ >> as ge ->
         try
           let pwel = un_extfun [] list in
-          let s1 =
-            sprintf "%sextfun %s with" pc.bef
-              (expr {(pc) with bef = ""; aft = ""} e)
-          in
-          let s2 = match_assoc_list {(pc) with bef = tab pc.ind} pwel in
-          sprintf "%s\n%s" s1 s2
+          pprintf pc "@[<b>extfun %p with@ %p" expr e match_assoc_list pwel
         with
         [ Not_found -> next pc ge ] ] ]
   ;
