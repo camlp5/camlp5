7cb4c93360caba069486c5e8e2bcb7837eadef0a
deraugla <deraugla>
2010-08-24 00:36:07 +0000

    -

--- tools/camlp5_comm.sh
+++ tools/camlp5_comm.sh
@@ -1,40 +1,38 @@
 #!/bin/sh
-# $Id: camlp5_comm.sh,v 1.11 2010/08/23 08:57:30 deraugla Exp $
+# $Id: camlp5_comm.sh,v 1.12 2010/08/24 00:36:07 deraugla Exp $
 
 ARGS1="-mode $MODE"
 FILE=
 QUIET=no
 while test "" != "$1"; do
-	case $1 in
-	-q) QUIET=yes;;
-	*.ml*) FILE=$1;;
-	*) ARGS1="$ARGS1 $1";;
-	esac
-	shift
+  case $1 in
+  -q) QUIET=yes;;
+  *.ml*) FILE=$1;;
+  *) ARGS1="$ARGS1 $1";;
+  esac
+  shift
 done
 
 head -1 $FILE >/dev/null || exit 1
 
 set - `head -1 $FILE`
 if test "$2" = "camlp5r" -o "$2" = "camlp5"; then
-	WHAT="$2"
-	COMM="ocamlrun$EXE ../boot/$WHAT$EXE -nolib -I ../boot"
-        dir=`dirname $OTOP`
-        if test "`basename $dir`" != "ocaml_stuff"; then
-            COMM="$OTOP/boot/$COMM"
-        fi
-	shift; shift
-	ARGS2=`echo $* | sed -e "s/[()*]//g"`
-#	ARGS2=`echo $ARGS2 | sed -e "s/q_MLast.cmo/q_ast.cmo/"`
-#	ARGS1="$ARGS1 -verbose"
-	if test "$QUIET" = "no"; then echo $COMM $ARGS2 $ARGS1 $FILE; fi
-	$COMM $ARGS2 $ARGS1 $FILE
+  WHAT="$2"
+  COMM="ocamlrun$EXE ../boot/$WHAT$EXE -nolib -I ../boot"
+  dir=`dirname $OTOP`
+  if test "`basename $dir`" != "ocaml_stuff"; then
+    COMM="$OTOP/boot/$COMM"
+  fi
+  shift; shift
+  ARGS2=`echo $* | sed -e "s/[()*]//g"`
+  if test "$QUIET" = "no"; then echo $COMM $ARGS2 $ARGS1 $FILE; fi
+  $COMM $ARGS2 $ARGS1 $FILE
 else
-	if test "`basename $FILE .mli`.mli" = "$FILE"; then
-		OFILE=`basename $FILE .mli`.ppi
-	else
-		OFILE=`basename $FILE .ml`.ppo
-	fi
-	if test "$QUIET" = "no"; then echo cp $FILE $OFILE; fi
-	cp $FILE $OFILE
+  if test "`basename $FILE .mli`.mli" = "$FILE"; then
+    OFILE=`basename $FILE .mli`.ppi
+  else
+    OFILE=`basename $FILE .ml`.ppo
+  fi
+  if test "$QUIET" = "no"; then echo cp $FILE $OFILE; fi
+  cp $FILE $OFILE
 fi
