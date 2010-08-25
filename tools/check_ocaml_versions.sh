3cdf02136fc70b85d5f3cb1f235eae6d9bc42925
deraugla <deraugla>
2010-08-25 16:02:17 +0000

    -

--- tools/check_ocaml_versions.sh
+++ tools/check_ocaml_versions.sh
@@ -1,13 +1,14 @@
 #!/bin/sh -e
-# $Id: check_ocaml_versions.sh,v 1.6 2010/08/25 14:29:36 deraugla Exp $
+# $Id: check_ocaml_versions.sh,v 1.7 2010/08/25 16:02:17 deraugla Exp $
 
 TOP=$HOME/work
+DEST=$TOP/usr
 OCAMLSDIR=$TOP/ocaml/release
 CAMLP5DIR=$TOP/camlp5
 MODE=--strict
 
-cd $TOP
-PATH=$(pwd)/usr/bin:$PATH
+cd $DEST
+PATH=$(pwd)/bin:$PATH
 
 cd $OCAMLSDIR
 dirs=$(ls | grep -v '^[1|2]' | grep -v '^3.0[0-5]' | grep -v csl)
@@ -24,7 +25,7 @@ for i in $dirs; do
   echo "+++++ make clean"
   make clean
   echo "+++++ ./configure -bindir $TOP/usr/bin -libdir $TOP/usr/lib/ocaml -mandir $TOP/usr/man"
-  ./configure -bindir $TOP/usr/bin -libdir $TOP/usr/lib/ocaml -mandir $TOP/usr/man
+  ./configure -bindir $DEST/bin -libdir $DEST/lib/ocaml -mandir $DEST/man
   sed -e 's/ graph//' config/Makefile > tmp; mv tmp config/Makefile
   if [ "$i" = "3.06" ]; then
     echo "+++++ time make world"
