1030ebe9b77d1a50f5010dfe9b4429d9099e004f
deraugla <deraugla>
2010-09-01 20:17:09 +0000

    -

--- tools/mkcrc.sh
+++ tools/mkcrc.sh
@@ -1,10 +1,12 @@
 #!/bin/sh -e
-# $Id: mkcrc.sh,v 1.18 2010/09/01 16:44:47 deraugla Exp $
+# $Id: mkcrc.sh,v 1.19 2010/09/01 20:17:09 deraugla Exp $
 
 V=$OVERSION
 
 MOD_OLIB="arg array"
-if [ "$V" != "2.01" ]; then MOD_OLIB="$MOD_OLIB buffer"; fi
+if [ "$V" != "2.00" -a "$V" != "2.01" ]; then
+  MOD_OLIB="$MOD_OLIB buffer"
+fi
 MOD_OLIB="$MOD_OLIB char filename format hashtbl lexing list obj pervasives printf set sort stream string sys"
 MOD_MAIN="ast2pt exparser mLast parserify pcaml prtools quotation reloc"
 MOD_5LIB="diff eprinter extfun fstream gramext grammar plexer plexing ploc pprintf pretty versdep"
@@ -13,9 +15,10 @@ MOD_UTIL="pconfig warnings"
 OFILE=crc.tmp
 
 > $OFILE
-if [ "$V" = "2.01" -o "$V" = "2.02" -o "$V" = "2.03" -o "$V" = "2.04" -o \
-     "$V" = "2.99" -o "$V" = "3.00" -o "$V" = "3.01" -o "$V" = "3.02" -o \
-     "$V" = "3.03" -o "$V" = "3.04" -o "$V" = "3.05" -o "$V" = "3.06" ]
+if [ "$V" = "2.00" -o "$V" = "2.01" -o "$V" = "2.02" -o "$V" = "2.03" -o \
+     "$V" = "2.04" -o "$V" = "2.99" -o "$V" = "3.00" -o "$V" = "3.01" -o \
+     "$V" = "3.02" -o "$V" = "3.03" -o "$V" = "3.04" -o "$V" = "3.05" -o \
+     "$V" = "3.06" ]
 then
   (cd $OLIBDIR; $OLIBDIR/extract_crc $MOD_OLIB) >> $OFILE
   echo "in Dynlink.add_available_units crc_unit_list;;" >> $OFILE
