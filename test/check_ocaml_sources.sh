6cfeef0c08d68147ba8c9e4c4b7b7aff8b23637f
deraugla <deraugla>
2010-09-06 17:28:54 +0000

    -

--- test/check_ocaml_sources.sh
+++ test/check_ocaml_sources.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: check_ocaml_sources.sh,v 1.7 2010/09/06 17:16:26 deraugla Exp $
+# $Id: check_ocaml_sources.sh,v 1.8 2010/09/06 17:28:54 deraugla Exp $
 
 dir='../ocaml/trunk'
 
@@ -33,35 +33,36 @@ fi
 for i in $files; do
   echo ===============================
   echo $i
-  bname=$(basename $i$)
-  if [ "$bname" = "frx_fileinput.ml" ]; then
-    echo "skipping $bname$"
-  fi
+  bname=$(basename $i)
   idir=$(basename $(dirname $i))
-  syntax="etc/pa_o.cmo"
-  altsyntax="meta/pa_r.cmo"
-  syntname="normal syntax"
-  altsyntname="revised syntax"
-  if [ "$idir" = "Camlp4Printers" ]; then
-    syntax="meta/pa_r.cmo"
-    altsyntax="etc/pa_o.cmo"
-    syntname="revised syntax"
-    altsyntname="normal syntax"
+  if [ "$bname" = "frx_fileinput.ml" -o "$idir" = "builtin" ]; then
+    echo "skipping $bname"
+  else
+    syntax="etc/pa_o.cmo"
+    altsyntax="meta/pa_r.cmo"
+    syntname="normal syntax"
+    altsyntname="revised syntax"
+    if [ "$idir" = "Camlp4Printers" ]; then
+      syntax="meta/pa_r.cmo"
+      altsyntax="etc/pa_o.cmo"
+      syntname="revised syntax"
+      altsyntname="normal syntax"
+    fi
+    echo "*** testing $syntname to revised syntax"
+    main/camlp5 $syntax -I etc pr_r.cmo pr_ro.cmo "$i" > /tmp/t1.ml
+    echo "*** testing revised syntax on itself"
+    main/camlp5 meta/pa_r.cmo -I etc pr_r.cmo pr_ro.cmo /tmp/t1.ml |
+    diff -c /tmp/t1.ml -
+    echo "*** testing revised syntax to normal syntax"
+    main/camlp5 meta/pa_r.cmo -I etc pr_o.cmo /tmp/t1.ml > /tmp/t2.ml
+    echo "*** testing $syntname to normal syntax (result: t2.ml)"
+    main/camlp5 $syntax -I etc pr_o.cmo "$i" > /tmp/t3.ml
+    echo "*** testing normal syntax on itself (result: t3.ml)"
+    main/camlp5 etc/pa_o.cmo -I etc pr_o.cmo /tmp/t3.ml | diff /tmp/t3.ml -
+    echo "*** comparing t2.ml and t3.ml"
+    diff /tmp/t2.ml /tmp/t3.ml || :
+    echo "*** testing $syntname to OCaml parse tree"
+    main/camlp5 $syntax meta/pr_dump.cmo "$i" > /dev/null
   fi
-  echo "*** testing $syntname to revised syntax"
-  main/camlp5 $syntax -I etc pr_r.cmo pr_ro.cmo "$i" > /tmp/t1.ml
-  echo "*** testing revised syntax on itself"
-  main/camlp5 meta/pa_r.cmo -I etc pr_r.cmo pr_ro.cmo /tmp/t1.ml |
-  diff -c /tmp/t1.ml -
-  echo "*** testing revised syntax to normal syntax"
-  main/camlp5 meta/pa_r.cmo -I etc pr_o.cmo /tmp/t1.ml > /tmp/t2.ml
-  echo "*** testing $syntname to normal syntax (result: t2.ml)"
-  main/camlp5 $syntax -I etc pr_o.cmo "$i" > /tmp/t3.ml
-  echo "*** testing normal syntax on itself (result: t3.ml)"
-  main/camlp5 etc/pa_o.cmo -I etc pr_o.cmo /tmp/t3.ml | diff /tmp/t3.ml -
-  echo "*** comparing t2.ml and t3.ml"
-  diff /tmp/t2.ml /tmp/t3.ml || :
-  echo "*** testing $syntname to OCaml parse tree"
-  main/camlp5 $syntax meta/pr_dump.cmo "$i" > /dev/null
 done
 # rm -f /tmp/t1.ml /tmp/t2.ml /tmp/t3.ml
