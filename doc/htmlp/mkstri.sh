603e2ef5283fa58fea151ed731d08cb78fdb3277
deraugla <deraugla>
2010-09-15 07:48:40 +0000

    -

--- doc/htmlp/mkstri.sh
+++ doc/htmlp/mkstri.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: mkstri.sh,v 1.1 2010/09/15 03:39:01 deraugla Exp $
+# $Id: mkstri.sh,v 1.2 2010/09/15 07:48:40 deraugla Exp $
 
 top=../..
 quotation="$1"
@@ -7,5 +7,5 @@ quotation="$1"
 $top/meta/camlp5r $top/meta/q_MLast.cmo $top/etc/pr_r.cmo -l200 -impl $top/test/quot_r.ml |
 paste $top/test/quot_r.ml - |
 grep "<:$quotation<" |
-sed -e 's/;$//; s/</\&lt;/g; s/^/    <tt style="color:blue">/; s/MLast/    <tt style="color:red">MLast/; s|>>;|>></tt><br/>|; s|$|</tt><br/>          <br/>|' |
+sed -e 's/;$//; s/</\&lt;/g; s/^/    <tt style="color:blue">/; s/MLast/    <tt style="color:red">MLast/; s|>>;|>></tt><br/>|; s|$|</tt><br/>	    <br/>|' |
 tr '\t' '\n'
