bcedc78619b71b500b78ea2d03f42d5a47a91ea2
deraugla <deraugla>
2010-09-17 20:37:30 +0000

    -

--- doc/htmlp/mkstri.sh
+++ doc/htmlp/mkstri.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: mkstri.sh,v 6.9 2010/09/17 16:05:42 deraugla Exp $
+# $Id: mkstri.sh,v 6.10 2010/09/17 20:37:30 deraugla Exp $
 
 top=../..
 file=$top/test/quot_r.ml
@@ -12,7 +12,7 @@ echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <!-- $Id: mkstri.sh,v 6.9 2010/09/17 16:05:42 deraugla Exp $ -->
+  <!-- $Id: mkstri.sh,v 6.10 2010/09/17 20:37:30 deraugla Exp $ -->
   <!-- Copyright (c) INRIA 2007-2010 -->
   <title>AST - strict</title>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -75,23 +75,29 @@ for q in $quotation_list; do
   fi
 
   class=' class="nodelist"'
-  if [ "$q" = "type_decl" ]; then class=""; fi
+  if [ "$q" = "type_decl" ]; then
+    class="";
+    echo '<dl>'
+    echo '  <dd>'
+  fi
 
   $top/meta/camlp5r -nolib $top/meta/q_MLast.cmo $top/etc/pr_r.cmo -l200 -impl $top/test/quot_r.ml |
   paste -d@ $file - |
   sed -e 's/(\*.*\*)@//; /\*)$/N; s/\n//' |
-  sed -e '/(\*/{s/(\*/(* -/; h; s/\*).*$/\*)/; x}; /^</{G; s/^\(.*\)\n\(.*\)$/\2\1/}' |
-  sed -e '/@{/s/(\*.*\*)/(*  *)/' |
   grep "<:$q<" |
-  sed -e 's/;$//; s/&/&amp;/g; s/<:/\&lt;:/g; s/< /\&lt; /g' |
-  sed -e "s/^/<dl$class>@/" |
-  sed -e 's|(\* |  <dt>|; s| \*)|</dt>@|' |
-  sed -e 's/@&/@  <dd>@    <tt style="color:blue">\&/' |
-  sed -e 's/@MLast/@    <tt style="color:red">MLast/' |
-  sed -e 's/@{MLast/@    <tt style="color:red">{MLast/' |
-  sed -e 's|>>;|>></tt><br/>|' |
-  sed -e 's|$|</tt>@  </dd>@</dl>|' |
+  sed -e "s|(\* |  </dd>@</dl>@<dl$class>@  <dt>- |" |
+  sed -e 's| \*)|</dt>@  <dd>@    <tt style="color:blue">|' |
+  sed -e 's|<:\([a-z_]*\)<|\&lt;:\1\&lt;|' |
+  sed -e 's|{<|{\&lt;|' |
+  sed -e 's| >>;@| >></tt><br/>@    <tt style="color:red">|' |
+  sed -e 's|;$|</tt>|' |
+  sed -e 's|^&|    <br/><br/>@    <tt style="color:blue">\&|' |
+  sed -e '1s/^[^@]*@[^@]*@//' |
   tr '@' '\n'
+
+  echo '  </dd>'
+  echo '</dl>'
+  echo
 
 done
 
