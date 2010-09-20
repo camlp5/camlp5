cc305865a97cab129dd4964751cd4b317670e667
deraugla <deraugla>
2010-09-20 10:11:10 +0000

    -

--- doc/htmlp/mktrans.sh
+++ doc/htmlp/mktrans.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: mktrans.sh,v 6.14 2010/09/17 16:05:42 deraugla Exp $
+# $Id: mktrans.sh,v 6.15 2010/09/20 10:11:10 deraugla Exp $
 
 top=../..
 file=$top/test/quot_r.ml
@@ -12,7 +12,7 @@ echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <!-- $Id: mktrans.sh,v 6.14 2010/09/17 16:05:42 deraugla Exp $ -->
+  <!-- $Id: mktrans.sh,v 6.15 2010/09/20 10:11:10 deraugla Exp $ -->
   <!-- Copyright (c) INRIA 2007-2010 -->
   <title>AST - transitional</title>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -105,6 +105,7 @@ for q in $quotation_list; do
   echo '</table>'
 done
 
+echo
 echo '<div class="trailer">
 </div>
 
