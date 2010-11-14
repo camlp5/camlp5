6796b63aa39c21bce84da07b0dbacc9c43c84e4c
deraugla <deraugla>
2010-11-14 18:22:12 +0000

    -

--- main/pcaml.ml
+++ main/pcaml.ml
@@ -1,5 +1,5 @@
 (* camlp5r *)
-(* $Id: pcaml.ml,v 6.13 2010/11/14 17:10:37 deraugla Exp $ *)
+(* $Id: pcaml.ml,v 6.14 2010/11/14 18:22:12 deraugla Exp $ *)
 (* Copyright (c) INRIA 2007-2010 *)
 
 #load "pa_macro.cmo";
@@ -7,7 +7,7 @@
 
 open Printf;
 
-value version = "6.02";
+value version = "6.03-exp";
 value syntax_name = ref "";
 
 value gram =
--- ocaml_src/main/pcaml.ml
+++ ocaml_src/main/pcaml.ml
@@ -7,7 +7,7 @@
 
 open Printf;;
 
-let version = "6.02";;
+let version = "6.03-exp";;
 let syntax_name = ref "";;
 
 let gram =
