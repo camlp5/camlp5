8426baaf6c796af2d0ddb6e2c90a165fdb817780
deraugla <deraugla>
2013-03-15 16:17:17 +0000

    -

--- main/exparser.ml
+++ main/exparser.ml
@@ -1,5 +1,5 @@
 (* camlp5r *)
-(* $Id: exparser.ml,v 6.6 2013/03/14 15:43:42 deraugla Exp $ *)
+(* $Id: exparser.ml,v 6.7 2013/03/15 16:17:17 deraugla Exp $ *)
 (* Copyright (c) INRIA 2007-2012 *)
 
 #load "q_MLast.cmo";
@@ -351,7 +351,7 @@ value left_factorize rl =
 (* Converting into AST *)
 
 value cparser loc bpo pc =
-  let pc = if optim.val then left_factorize pc else pc in
+  let pc = left_factorize pc in
   let e = parser_cases loc pc in
   let e =
     let loc = Ploc.with_comment loc "" in
@@ -374,7 +374,7 @@ value rec is_not_bound s =
 ;
 
 value cparser_match loc me bpo pc =
-  let pc = if optim.val then left_factorize pc else pc in
+  let pc = left_factorize pc in
   let iloc = Ploc.with_comment loc "" in
   let pc = parser_cases iloc pc in
   let e =
