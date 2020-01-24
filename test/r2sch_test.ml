fcdf613f27e715fb0a16d1f1473f736d1d045f9d
Chet Murthy <chetsky@gmail.com>
2020-01-24 12:39:44 -0800

    meh

--- test/r2sch_test.ml
+++ test/r2sch_test.ml
@@ -4,12 +4,14 @@ open OUnit2
 open OUnitTest
 ;;
 
+Pcaml.inter_phrases := Some ("\n") ;;
+
 let tests = "test pa_r -> pr_scheme" >::: [
     "simplest" >:: (fun _ ->
-        assert_equal
-          {|(begin 1 2) ;
-3 ;
-(define x 1) ;
+        assert_equal ~msg:"not equal" ~printer:(fun x -> x)
+          {|(begin 1 2)
+3
+(define x 1)
 |}
           (pr (pa "do { 1; 2 }; 3 ; value x = 1 ;"))
       )
--- test/sch2r_test.ml
+++ test/sch2r_test.ml
@@ -4,11 +4,9 @@ open OUnit2
 open OUnitTest
 ;;
 
-Pcaml.inter_phrases := None ;;
-
 let tests = "test pa_scheme -> pr_r" >::: [
     "simplest" >:: (fun _ ->
-        assert_equal
+        assert_equal ~msg:"not equal" ~printer:(fun x -> x)
           {|do { 1; 2 } ;
 3 ;
 value x = 1 ;
@@ -16,9 +14,9 @@ value x = 1 ;
           (pr (pa {|(begin 1 2)  3  (define x 1)|}))
       )
 ]
-(*
+
 let _ = run_test_tt_main tests ;;
-*)  
+
 (*
 ;;; Local Variables: ***
 ;;; mode:tuareg ***
