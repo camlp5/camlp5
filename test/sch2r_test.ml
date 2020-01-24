fff5de60449dea293f123081e7f40e5d7bba866e
Chet Murthy <chetsky@gmail.com>
2020-01-24 12:32:30 -0800

    unit-tests for:   scheme -> r   r -> scheme

--- test/sch2r_test.ml
+++ test/sch2r_test.ml
@@ -4,19 +4,21 @@ open OUnit2
 open OUnitTest
 ;;
 
-let tests = "test pa_r -> pr_scheme" >::: [
+Pcaml.inter_phrases := None ;;
+
+let tests = "test pa_scheme -> pr_r" >::: [
     "simplest" >:: (fun _ ->
         assert_equal
-          {|(begin 1 2) ;
+          {|do { 1; 2 } ;
 3 ;
-(define x 1) ;
+value x = 1 ;
 |}
-          (pr (pa "do { 1; 2 }; 3 ; value x = 1 ;"))
+          (pr (pa {|(begin 1 2)  3  (define x 1)|}))
       )
 ]
-
+(*
 let _ = run_test_tt_main tests ;;
-  
+*)  
 (*
 ;;; Local Variables: ***
 ;;; mode:tuareg ***
--- test/testutil.ml
+++ test/testutil.ml
@@ -1,10 +1,59 @@
 (* camlp5r *)
 (* testutil.ml,v *)
 
+open Printf;
+
+value print_location loc =
+  let loc =
+    if Ploc.file_name loc = "" then
+      Ploc.make_loc Pcaml.input_file.val 1 0 (0, 1) ""
+    else loc
+  in
+  let fname = Ploc.file_name loc in
+  let bp = Ploc.first_pos loc in
+  let ep = Ploc.last_pos loc in
+  if fname <> "-" then
+    let line = Ploc.line_nb loc in
+    let bol = Ploc.bol_pos loc in
+    eprintf "%s"
+      (Pcaml.string_of_loc fname line (bp - bol + 1) (ep - bol + 1))
+  else
+    eprintf "At location %d-%d\n" bp ep
+;
+
+value report_error =
+  fun
+  [ Odyl_main.Error fname msg -> do {
+      Format.print_string "Error while loading \"";
+      Format.print_string fname;
+      Format.print_string "\": ";
+      Format.print_string msg
+    }
+  | exc -> Pcaml.report_error exc ]
+;
+
+value report_error_and_exit exc = do {
+  Format.set_formatter_out_channel stderr;
+  Format.open_vbox 0;
+  let exc =
+    match exc with
+    [ Ploc.Exc loc exc -> do { print_location loc; exc }
+    | _ -> exc ]
+  in
+  report_error exc;
+  Format.close_box ();
+  Format.print_newline ();
+  exit 2
+};
+
+value wrap_err f arg =
+try f arg with exc -> report_error_and_exit exc
+;
+
 value pa s = let (ast, _) = Pcaml.parse_implem.val (Stream.of_string s) in ast ;
 Pcaml.inter_phrases.val := Some " ;\n" ;
-value sep = match Pcaml.inter_phrases.val with [ None -> "" | Some s -> s ] ;
 value pr l = 
+  let sep = match Pcaml.inter_phrases.val with [ None -> "" | Some s -> s ] in
   let b = Buffer.create 23 in do {
     List.iter (fun (ast, _) -> 
       let s = Eprinter.apply Pcaml.pr_str_item Pprintf.empty_pc ast in do {
