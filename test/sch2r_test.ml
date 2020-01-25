e5ee0862dac265f4c9711b862fc183cf72d30ee2
Chet Murthy <chetsky@gmail.com>
2020-01-24 22:09:04 -0800

    tests pass

--- test/sch2r_test.ml
+++ test/sch2r_test.ml
@@ -5,23 +5,7 @@ open OUnit2
 open OUnitTest
 ;;
 
-let pa_sexpr s = Grammar.Entry.parse Pa_schemer.sexpr (Stream.of_string s);;
-let pa_str_item s = Grammar.Entry.parse Pcaml.str_item (Stream.of_string s);;
-let pa_implem s = Grammar.Entry.parse Pcaml.implem (Stream.of_string s);;
-
 let tests = "test pa_scheme -> pr_r" >::: [
-    "sexpr" >:: (fun _ ->
-          let _ = pa_sexpr {|(begin 1 2)  3  (define x 1)|} in ();
-      );
-    "str_item" >:: (fun _ ->
-          let _ = pa_str_item {|(module M (struct (define x 1)))|} in ();
-      );
-    "implem" >:: (fun _ ->
-          let _ = pa_implem {|(module M (struct (define x 1)))|} in ();
-      );
-    "str_item empty module" >:: (fun _ ->
-          let _ = pa_str_item {|(module M (struct ))|} in ();
-      );
     "simplest" >:: (fun _ ->
         assert_equal ~msg:"not equal" ~printer:(fun x -> x)
           {|do { 1; 2 } ;
@@ -38,9 +22,9 @@ value x = 1 ;
       );
     "empty module" >:: (fun _ ->
         assert_equal ~msg:"not equal" ~printer:(fun x -> x)
-          {|module M = struct value x = 1; end ;
+          {|module M = struct  end ;
 |}
-          (pr (exn_wrap_result pa {|(module M (struct )|}))
+          (pr (exn_wrap_result pa {|(module M (struct ))|}))
       );
 
 ]
