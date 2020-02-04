3ee44fad35cf3d1cff0e4e83afc119359fd6f10b
Chet Murthy <chetsky@gmail.com>
2020-02-03 18:42:13 -0800

    switch sphinx theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -74,14 +74,13 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-
+html_theme = "bizstyle"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {'navigation_depth': 6}
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
