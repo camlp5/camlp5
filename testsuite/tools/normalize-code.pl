172c218b0490897efbaaa78d98bf75f4211b53b2
Chet Murthy <chetsky@gmail.com>
2020-02-08 19:02:40 -0800

    starting to use ocamlformat for normalizing original-syntax

--- testsuite/tools/normalize-code.pl
+++ testsuite/tools/normalize-code.pl
@@ -1,26 +1,48 @@
 #!/usr/bin/perl
 
+use Data::Dumper ;
+use Carp::Assert ;
+use Getopt::Long;
+use IPC::System::Simple qw(systemx runx capturex $EXITVAL);
+use String::ShellQuote ;
+
+our $verbose = 0 ;
+our $syntax ;
+
+our $ocamlformat = "/home/chet/Hack/Ocaml/GENERIC/4.09.0/bin/ocamlformat" ;
+
 {
-  my $syntax ;
-  if ($ARGV[0] eq '-syntax') {
-    shift @ARGV ;
-    $syntax = shift @ARGV ;
-  }
+  GetOptions(
+    "syntax=s" => \$syntax,
+    "verbose" => \$verbose,
+      )
+      or die("Error in command line arguments\n");
 
   my $src = shift @ARGV ;
   my $dst = shift @ARGV ;
-  my $txt = f_read($src) ;
-  $txt =~ s|^#load.*$||gm ;
-  $txt =~ s|\(\*.*?\*\)| onlynl($&) |gse ;
-  $txt =~ s,→,->,gs ;
-  $txt =~ s,declare end;,,gm ;
   if ($syntax eq 'revised') {
+    my $txt = f_read($src) ;
+    $txt =~ s|^#load.*$||gm ;
+    $txt =~ s|\(\*.*?\*\)| onlynl($&) |gse ;
+    $txt =~ s,→,->,gs ;
+    $txt =~ s,declare end;,,gm ;
     $txt =~ s,;(?:\s*;)*,;,gs ;
+    f_write($dst, $txt) ; 
   }
   else {
-    $txt =~ s,;;,,gs ;
+    my $tyflag = "--impl" ;
+    $tyflag = "--intf" if ($src =~ m/\.mli/) ;
+
+    my $txt = f_read($src) ;
+    $txt =~ s|\(\*.*?\*\)| onlynl($&) |gse ;
+    $txt =~ s,→,->,gs ;
+    f_write($src, $txt) ;
+
+    my $txt = v_capturex($ocamlformat, 
+			 "--enable-outside-detected-project", "--no-comment-check",
+			 $tyflag, $src) ;
+    f_write($dst, $txt) ; 
   }
-  f_write($dst, $txt) ; 
 }
 
 sub onlynl {
@@ -42,4 +64,31 @@ sub f_write {
   open(F,">$f") || die "cannot open $f for writing";
   print F @_;
   close(F);
+}
+sub v_systemx {
+  my $codes ;
+  $codes = shift if (ref($_[0]) eq 'ARRAY') ;
+  my @cmd = @_ ;
+  print STDERR join(' ', map { shell_quote($_) } @cmd)."\n" if ($main::verbose) ;
+
+  if ($codes) {
+    return runx($codes, @cmd) ;
+  }
+  else {
+    return runx(@cmd) ;
+  }
+}
+
+sub v_capturex {
+  my $codes ;
+  $codes = shift if (ref($_[0]) eq 'ARRAY') ;
+  my @cmd = @_ ;
+  print STDERR join(' ', map { shell_quote($_) } @cmd)."\n" if ($main::verbose) ;
+
+  if ($codes) {
+    return capturex($codes, @cmd) ;
+  }
+  else {
+    return capturex(@cmd) ;
+  }
 }
