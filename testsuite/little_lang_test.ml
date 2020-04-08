6d0504c3d6131d1f3160781cfeede8eef26feb8e
Chet Murthy <chetsky@gmail.com>
2020-04-08 13:50:52 -0700

    meh

--- testsuite/little_lang_test.ml
+++ testsuite/little_lang_test.ml
@@ -5,6 +5,9 @@
 #load "pa_extend.cmo";
 #load "q_MLast.cmo";
 #load "pa_macro.cmo";
+#load "pa_extfun.cmo";
+#load "pa_extprint.cmo";
+#load "pa_pprintf.cmo";
 
 open Testutil;
 open Testutil2;
@@ -18,6 +21,7 @@ type expr = [
   | EPOW of expr and expr ]
 ;
 
+module Pa = struct
 value gram = Grammar.gcreate (Plexer.gmake ());
 
 value expression = Grammar.Entry.create gram "expression";
@@ -48,9 +52,37 @@ END
 
 value expression_top s = s |> Stream.of_string |> Grammar.Entry.parse expression ;
 
+end ;
+
+module Pr = struct
+value pr_expression = Eprinter.make "expression";
+value expression = Eprinter.apply pr_expression;
+
+EXTEND_PRINTER
+  pr_expression:
+    [ "add"
+      [ EADD x y -> pprintf pc "%p + %p" curr x curr y
+      | ESUB x y -> pprintf pc "%p - %p" curr x curr y ]
+    | "mul"
+      [ EMUL x y -> pprintf pc "%p * %p" curr x curr y
+      | EDIV x y -> pprintf pc "%p / %p" curr x curr y ]
+    | "pow"
+      [ EPOW x y -> pprintf pc "%p ** %p" curr x curr y ]
+    | "simple"
+      [ EINT i -> pprintf pc "%s" i ]
+    | "bottom"
+      [ z -> pprintf pc "(%p)" expression z ]
+    ]
+  ;
+END;
+
+end ;
+
+value roundtrip s = s |> Pa.expression_top |> (Pr.expression Pprintf.empty_pc) ;
+
 open OUnit2;
 
-value tests () = "little_lang" >::: [
+value parse_tests = "parse" >::: Pa.[
     "expr-top-simple" >:: (fun [ _ ->
       assert_equal (EINT "1") (expression_top "1")
     ])
@@ -72,11 +104,37 @@ value tests () = "little_lang" >::: [
     ; "expr-top-add-pow" >:: (fun [ _ ->
       assert_equal (EADD (EINT "1") (EPOW (EINT "2") (EINT "3"))) (expression_top "1 + 2 ** 3")
     ])
+    ; "roundtrip-top-add-pow" >:: (fun [ _ ->
+      assert_equal (EADD (EINT "1") (EPOW (EINT "2") (EINT "3"))) (expression_top "1 + 2 ** 3")
+    ])
+]
+;
+
+value rt name s = 
+  name >:: (fun [ _ ->
+    assert_equal ~{printer=fun x -> x} s (roundtrip s)
+  ])
+;
+
+value roundtrip_tests = "roundtrip" >::: Pa.[
+    rt "expr-top-simple" "1"
+    ; rt "expr-top-pow" "1 ** 2"
+    ; rt "expr-top-mul" "1 * 2"
+    ; rt "expr-top-div" "1 / 2"
+    ; rt "expr-top-sub" "1 - 2"
+    ; rt "expr-top-add" "1 + 2"
+    ; rt "expr-top-add-pow" "1 + 2 ** 3"
+]
+;
+
+value tests = "little_lang" >::: Pa.[
+    "parse" >: parse_tests ;
+    "roundtrip" >: roundtrip_tests
 ]
 ;
 
 value _ = 
 if invoked_with "little_lang_test" then
-  run_test_tt_main (tests ())
+  run_test_tt_main tests
 else ()
 ;
