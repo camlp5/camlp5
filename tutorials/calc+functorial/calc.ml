a70c636c1bce2718baaf4d6e3480d3a3d0c35170
Chet Murthy <chetsky@gmail.com>
2020-06-16 12:40:52 -0700

    meh
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch 27.documentation
    # Your branch is up-to-date with 'my-fork/27.documentation'.
    #
    # Changes to be committed:
    #       new file:   tutorials/calc+functorial/.gitignore
    #       new file:   tutorials/calc+functorial/Makefile
    #       new file:   tutorials/calc+functorial/calc.ml
    #       new file:   tutorials/calc+functorial/calclexer.mll
    #
    # Untracked files:
    #       extra-tools/
    #       testsuite/papr_official.ppo.ml
    #

---
+++ tutorials/calc+functorial/calc.ml
@@ -0,0 +1,40 @@
+#load "pa_extend.cmo";
+
+module Ocamllex_L = struct
+type te = (string * string) ;
+value lexer = Plexing.lexer_func_of_ocamllex Calclexer.token ;
+value lexer = {Plexing.tok_func = lexer;
+ Plexing.tok_using _ = (); Plexing.tok_removing _ = ();
+ Plexing.tok_match = Plexing.default_match;
+ Plexing.tok_text = Plexing.lexer_text;
+ Plexing.tok_comm = None} ;
+end ;
+
+module Gram = Grammar.GMake(Ocamllex_L) ;
+
+value e = Gram.Entry.create "expression";
+
+GEXTEND Gram
+  e:
+    [ [ x = e; "+"; y = e -> x + y
+      | x = e; "-"; y = e -> x - y ]
+    | [ x = e; "*"; y = e -> x * y
+      | x = e; "/"; y = e -> x / y ]
+    | [ x = INT -> int_of_string x
+      | "("; x = e; ")" -> x ] ]
+  ;
+END;
+
+open Printf;
+
+try
+for i = 1 to Array.length Sys.argv - 1 do {
+  let r = Gram.Entry.parse e (Gram.parsable (Stream.of_string Sys.argv.(i))) in
+  printf "%s = %d\n" Sys.argv.(i) r;
+  flush stdout;
+}
+with [ Ploc.Exc loc exc ->
+    Fmt.(pf stderr "%s%a@.%!" (Ploc.string_of_location loc) exn exc)
+  | exc -> Fmt.(pf stderr "%a@.%!" exn exc)
+]
+;
