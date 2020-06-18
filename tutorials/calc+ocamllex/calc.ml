10018645a0d68225cb5b5f56fff851cc99a39913
Chet Murthy <chetsky@gmail.com>
2020-06-18 12:06:51 -0700

    meh
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch 28.documentation
    # Your branch is up-to-date with 'my-fork/28.documentation'.
    #
    # Changes to be committed:
    #       modified:   tutorials/calc+ocamllex/Makefile
    #       modified:   tutorials/calc+ocamllex/calc.ml
    #       modified:   tutorials/calc+ocamllex/calclexer.mll
    #
    # Untracked files:
    #       extra-tools/
    #       testsuite/papr_official.ppo.ml
    #       tutorials/calc+ocamllex/.depend
    #       tutorials/calc+ocamllex/TEST
    #       tutorials/calc+ocamllex/include_ml
    #       tutorials/calc+pa_lexer/TEST
    #       tutorials/calc+pa_lexer/include_ml
    #       tutorials/protobuf2/
    #

--- tutorials/calc+ocamllex/calc.ml
+++ tutorials/calc+ocamllex/calc.ml
@@ -1,4 +1,13 @@
+(*
 #load "pa_extend.cmo";
+#load "pa_lexer.cmo";
+#load "pa_extprint.cmo";
+#load "pa_pprintf.cmo";
+*)
+
+value input_file = ref "" ;
+value nonws_re = Pcre.regexp "\\S" ;
+value has_nonws s = Pcre.pmatch ~{rex=nonws_re} s;
 
 value lexer = Plexing.lexer_func_of_ocamllex Calclexer.token ;
 value lexer = {Plexing.tok_func = lexer;
@@ -7,30 +16,170 @@ value lexer = {Plexing.tok_func = lexer;
  Plexing.tok_text = Plexing.lexer_text;
  Plexing.tok_comm = None} ;
 
+type binop = [ ADD | SUB | DIV | MUL ] ;
+type unop = [ PLUS | MINUS ] ;
+type expr = [
+  BINOP of Ploc.t and binop and expr and expr
+| UNOP of Ploc.t and unop and expr
+| INT of Ploc.t and int
+| VAR of Ploc.t and string ]
+and stmt = [
+  ASSIGN of Ploc.t and string and expr
+| EXPR of Ploc.t and expr
+]
+;
+
+value loc_of_expr = fun [
+  BINOP loc _ _ _ -> loc
+| UNOP loc _ _ -> loc
+| INT loc _ -> loc
+| VAR loc _ -> loc
+]
+;
+value loc_of_stmt = fun [
+  ASSIGN loc _ _ -> loc
+| EXPR loc _ -> loc
+]
+;
+
 value g = Grammar.gcreate lexer;
-value e = Grammar.Entry.create g "expression";
+value expr = Grammar.Entry.create g "expression";
+value stmt = Grammar.Entry.create g "statement";
+value stmts = Grammar.Entry.create g "statements";
+value stmts_eoi = Grammar.Entry.create g "statements_eoi";
+
+value loc_strip_comment loc = Ploc.with_comment loc "" ;
+
+
+value check_id_coloneq =
+  Grammar.Entry.of_parser g "check_id_coloneq"
+    (fun strm ->
+       match Stream.npeek 2 strm with
+       [ [("IDENT", _); ("", ":=")] -> ()
+       | _ -> raise Stream.Failure ])
+;
 
 EXTEND
-  e:
-    [ [ x = e; "+"; y = e -> x + y
-      | x = e; "-"; y = e -> x - y ]
-    | [ x = e; "*"; y = e -> x * y
-      | x = e; "/"; y = e -> x / y ]
-    | [ x = INT -> int_of_string x
-      | "("; x = e; ")" -> x ] ]
+  GLOBAL: expr stmt stmts stmts_eoi check_id_coloneq ;
+  expr:
+    [ [ x = expr; "+"; y = expr -> BINOP (loc_strip_comment loc) ADD x y
+      | x = expr; "-"; y = expr -> BINOP (loc_strip_comment loc) SUB x y ]
+    | [ x = expr; "*"; y = expr -> BINOP (loc_strip_comment loc) MUL x y
+      | x = expr; "/"; y = expr -> BINOP (loc_strip_comment loc) DIV x  y ]
+    | [ "-" ; x = expr -> UNOP loc MINUS x
+      | "+" ; x = expr -> UNOP loc PLUS x ]
+    | [ x = INT -> INT loc (int_of_string x)
+      | x = IDENT -> VAR loc x
+      | "("; x = expr; ")" -> x
+      ]
+    ]
+  ;
+  stmt:
+    [ [ check_id_coloneq ; id = IDENT ; ":=" ; x = expr -> ASSIGN loc id x
+      | x = expr -> EXPR loc x ]
+    ]
+  ;
+  stmts : [ [ l = LIST1 stmt SEP ";" -> l ] ] ;
+  stmts_eoi : [ [ l = stmts ; EOI -> l ] ] ;
+END;
+
+value parse_expr = Grammar.Entry.parse expr ;
+value parse_stmt = Grammar.Entry.parse stmt ;
+value parse_stmts = Grammar.Entry.parse stmts ;
+value parse_stmts_eoi = Grammar.Entry.parse stmts_eoi ;
+
+value pr_expr = Eprinter.make "expr";
+value pr_stmt = Eprinter.make "stmt";
+value pr_stmts = Eprinter.make "stmts";
+
+Eprinter.clear pr_expr;
+Eprinter.clear pr_stmt;
+Eprinter.clear pr_stmts;
+
+value print_expr = Eprinter.apply pr_expr;
+value print_stmt = Eprinter.apply pr_stmt;
+value print_commented_stmt pc stmt =
+  let loc = loc_of_stmt stmt in
+  let comment = Ploc.comment loc in
+  let comment = if has_nonws comment then comment else "" in
+  let pp = (fun () -> pprintf pc "%s%p" comment print_stmt stmt) in
+    Pretty.horiz_vertic pp pp
+;
+
+value print_stmts = Eprinter.apply pr_stmts;
+
+value plist_semi f sh pc l =
+  let l = List.map (fun s -> (s, ";")) l in
+  pprintf pc "%p" (Prtools.plist f sh) l
+;
+
+EXTEND_PRINTER
+  pr_expr:
+    [ "add"
+      [ BINOP _ ADD x y -> pprintf pc "@[%p +@ %p@]" curr x next y
+      | BINOP _ SUB x y -> pprintf pc "@[%p -@ %p@]" curr x next y ]
+    | "mul"
+      [ BINOP _ MUL x y -> pprintf pc "@[%p *@ %p@]" curr x next y
+      | BINOP _ DIV x y -> pprintf pc "@[%p /@ %p@]" curr x next y ]
+    | "uminus"
+      [ UNOP _ PLUS x -> pprintf pc "+ %p" curr x
+      | UNOP _ MINUS x -> pprintf pc "- %p" curr x ]
+    | "simple"
+      [ INT _ x -> pprintf pc "%d" x
+      | x -> pprintf pc "@[<2>(%p)@]" print_expr x ]
+    ] ;
+  pr_stmt:
+    [ [ ASSIGN _ id e -> pprintf pc "@[%s := %p@]" id print_expr e
+      | EXPR _ e -> pprintf pc "@[%p@]" print_expr e ]
+    ]
+  ;
+  pr_stmts:
+    [ [ l -> pprintf pc "{@;%p@;}" (plist_semi print_commented_stmt 0) l ]
+    ]
   ;
 END;
 
+module Eval = struct
+value expr env e =
+  let rec erec = fun [
+    BINOP _ ADD x y -> (erec x)+(erec y)
+  | BINOP _ SUB x y -> (erec x)-(erec y)
+  | BINOP _ DIV x y -> (erec x)/(erec y)
+  | BINOP _ MUL x y -> (erec x)*(erec y)
+  | UNOP _ MINUS x -> -(erec x)
+  | UNOP _ PLUS x -> erec x
+  | INT _ x -> x
+  | VAR _ s -> List.assoc s env
+  ]
+  in erec e
+;
+value stmt env = fun [
+  ASSIGN _ s e ->
+    let v = expr env e in ([ (s, v) :: env ], v)
+| EXPR _ e -> (env, expr env e)
+]
+;
+
+value stmts env l =
+  List.fold_left (fun (env, acc) s -> let (env, v) = stmt env s in (env, [v :: acc])) (env, []) l ;
+end
+;
+
 open Printf;
 
+Pretty.line_length.val := 10 ;
+
+if not Sys.interactive.val then
 try
-for i = 1 to Array.length Sys.argv - 1 do {
-  let r = Grammar.Entry.parse e (Stream.of_string Sys.argv.(i)) in
-  printf "%s = %d\n" Sys.argv.(i) r;
-  flush stdout;
-}
+    let l = parse_stmts_eoi (Stream.of_channel stdin) in do {
+      let print_int pc n = pprintf pc "%d" n in
+      printf "%s" (pprintf Pprintf.empty_pc "%p =@;@[[%p]@]\n"
+                     print_stmts l
+                     (plist_semi print_int 2) (snd(Eval.stmts [] l)))
+    }
 with [ Ploc.Exc loc exc ->
     Fmt.(pf stderr "%s%a@.%!" (Ploc.string_of_location loc) exn exc)
   | exc -> Fmt.(pf stderr "%a@.%!" exn exc)
 ]
+else ()
 ;
--- tutorials/calc+ocamllex/calclexer.mll
+++ tutorials/calc+ocamllex/calclexer.mll
@@ -2,17 +2,23 @@
 
 }
 
-let ws = [' ' '\t']
+let ws = [' ' '\t' '\r' '\n']+
 let decimal_digit = ['0'-'9']
 let decimal = decimal_digit+
+let comment = "//" [^ '\n']* '\n'
+let ident = ['a'-'z' 'A'-'Z' '_'] ['a'-'z' 'A'-'Z' '_' '0'-'9']*
 
 rule token = parse
+| comment { token lexbuf }
+| ws     { token lexbuf }
 | "(" { ("","(") }
 | ")" { ("",")") }
 | "+" { ("","+") }
 | "-" { ("","-") }
 | "*" { ("","*") }
 | "/" { ("","/") }
+| ":=" { ("",":=") }
+| ";" { ("",";") }
 | decimal as dec { ("INT",dec) }
-| ws+     { token lexbuf }
+| ident as id { ("IDENT",id) }
 | eof { ("EOI","") }
