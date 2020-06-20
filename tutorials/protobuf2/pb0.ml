1c6c6dcee89aebba65da11affdfeb4840cbe31f8
Chet Murthy <chetsky@gmail.com>
2020-06-19 18:28:51 -0700

    meh
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch 28.documentation
    # Your branch is up-to-date with 'my-fork/28.documentation'.
    #
    # Changes to be committed:
    #       modified:   tutorials/protobuf2/pb0.ml
    #
    # Untracked files:
    #       extra-tools/
    #       testsuite/papr_official.ppo.ml
    #       tutorials/calc+ocamllex/.depend
    #       tutorials/calc+ocamllex/TEST
    #       tutorials/calc+ocamllex/include_ml
    #       tutorials/calc+pa_lexer/TEST
    #       tutorials/calc+pa_lexer/include_ml
    #       tutorials/protobuf2/TEST
    #       tutorials/protobuf2/include_ml
    #       tutorials/protobuf2/lang.ebnf
    #

--- tutorials/protobuf2/pb0.ml
+++ tutorials/protobuf2/pb0.ml
@@ -42,6 +42,7 @@ type type_t = [ DOUBLE | FLOAT | INT32 | INT64 | UINT32 | UINT64
 type message_field_t = { mf_label : label_t ; mf_typ : type_t ; mf_name : string ; mf_num : int ; mf_options : list (option_name_t * constant_t) } ;
 
 type int_or_max = [ MAX | NUM of int ] ;
+type range_t = (int * option int_or_max) ;
 
 type group_t = { group_label : label_t ; group_name : string ; group_num : int ; group_body : list message_member_t }
 and message_member_t = [
@@ -49,13 +50,21 @@ and message_member_t = [
 | MM_GROUP of Ploc.t and group_t
 | MM_ONEOF of Ploc.t and string and list oneof_member_t
 | MM_MAP of Ploc.t and type_t and type_t and string and int and list (option_name_t * constant_t)
-| MM_EXTENSIONS of Ploc.t and list (int * option int_or_max)
+| MM_EXTENSIONS of Ploc.t and list range_t
+| MM_RESERVED of Ploc.t and choice (list range_t) (list string)
 ]
 and oneof_field_t = { of_type : type_t ; of_name : string ; of_num : int ; of_options : list (option_name_t * constant_t) }
 and oneof_member_t = [
   OM_FIELD of Ploc.t and oneof_field_t
 | OM_OPTION of Ploc.t and (option_name_t * constant_t)
 | OM_EMPTY of Ploc.t
+]
+;
+
+type enum_member_t = [
+  EM_EMPTY of Ploc.t
+| EM_OPTION of Ploc.t and (option_name_t * constant_t)
+| EM_FIELD of Ploc.t and string and int and list (option_name_t * constant_t)
 ]
 ;
 
@@ -66,6 +75,7 @@ type stmt = [
 | PACKAGE of Ploc.t and fullident_t
 | OPTION of Ploc.t and option_name_t and constant_t
 | MESSAGE of Ploc.t and string and list message_member_t
+| ENUM of Ploc.t and string and list enum_member_t
 ]
 ;
 
@@ -75,6 +85,7 @@ value loc_of_member = fun [
 | MM_ONEOF loc _ _ -> loc
 | MM_MAP loc _ _ _ _ _ -> loc
 | MM_EXTENSIONS loc _ -> loc
+| MM_RESERVED loc _ -> loc
 ]
 ;
 
@@ -85,6 +96,13 @@ value loc_of_oneof_member = fun [
 ]
 ;
 
+value loc_of_enum_member = fun [
+  EM_EMPTY loc -> loc
+| EM_OPTION loc _ -> loc
+| EM_FIELD loc _ _ _ -> loc
+]
+;
+
 value loc_of_stmt = fun [
   EMPTY loc -> loc
 | SYNTAX loc _ -> loc
@@ -92,6 +110,7 @@ value loc_of_stmt = fun [
 | PACKAGE loc _ -> loc
 | OPTION loc _ _ -> loc
 | MESSAGE loc _ _ -> loc
+| ENUM loc _ _ -> loc
 ]
 ;
 
@@ -135,6 +154,12 @@ EXTEND
     | ty = typ ; n = ident ; "=" ; num = int ; opts = options ; ";" ->
       OM_FIELD loc {of_type = ty ; of_name = n ; of_num = num ; of_options = opts }
   ] ] ;
+  enum_member:
+  [ [ ";" -> EM_EMPTY loc
+    | "option" ; b = option_binding ; ";" -> EM_OPTION loc b
+    | n = ident ; "=" ; num = signed_int ; opts = options ; ";" ->
+      EM_FIELD loc n num opts
+  ] ] ;
   member:
   [ [ l = label ; t = typ ; n = ident ; "=" ; num = int ; opts = options ; ";" ->
       MM_FIELD loc { mf_label=l; mf_typ=t; mf_name=n; mf_num=num; mf_options = opts }
@@ -146,6 +171,8 @@ EXTEND
       MM_MAP loc keyty valty n num opts
     | "extensions" ; l = LIST1 range SEP "," ; ";" ->
       MM_EXTENSIONS loc l
+    | "reserved" ; l = LIST1 range SEP "," ; ";" -> MM_RESERVED loc (Left l)
+    | "reserved" ; l = LIST1 ident SEP "," ; ";" -> MM_RESERVED loc (Right l)
   ] ] ;
   range :
   [ [ n = int -> (n, None)
@@ -168,6 +195,8 @@ EXTEND
         OPTION loc n c
       | "message" ; n = ident ; "{" ; l = LIST0 member ; "}" ->
         MESSAGE loc n l
+      | "enum" ; n = ident ; "{" ; l = LIST1 enum_member ; "}" ->
+        ENUM loc n l
       ]
     ]
   ;
@@ -188,6 +217,7 @@ EXTEND
   fullident : [ [ fid = LIST1 ident SEP "." -> fid ] ] ;
   ident: [ [ id = LIDENT -> id | id = UIDENT -> id ] ] ;
   int : [ [ n = INT -> int_of_string n ] ] ;
+  signed_int : [ [ "-" ; n = INT -> -(int_of_string n) | n = INT -> int_of_string n ] ] ;
 END;
 
 value parse_member = Grammar.Entry.parse member ;
@@ -197,11 +227,13 @@ value parse_stmts_eoi = Grammar.Entry.parse stmts_eoi ;
 
 value pr_member = Eprinter.make "member";
 value pr_oneof_member = Eprinter.make "oneof_member";
+value pr_enum_member = Eprinter.make "enum_member";
 value pr_stmt = Eprinter.make "stmt";
 value pr_stmts = Eprinter.make "stmts";
 
 Eprinter.clear pr_member;
 Eprinter.clear pr_oneof_member;
+Eprinter.clear pr_enum_member;
 Eprinter.clear pr_stmt;
 Eprinter.clear pr_stmts;
 
@@ -220,6 +252,15 @@ value print_commented_oneof_member pc member =
   let comment = Ploc.comment loc in
   let comment = if has_nonws comment then comment else "" in
   let pp = (fun () -> pprintf pc "%s%p" comment print_oneof_member member) in
+    Pretty.horiz_vertic pp pp
+;
+
+value print_enum_member = Eprinter.apply pr_enum_member;
+value print_commented_enum_member pc member =
+  let loc = loc_of_enum_member member in
+  let comment = Ploc.comment loc in
+  let comment = if has_nonws comment then comment else "" in
+  let pp = (fun () -> pprintf pc "%s%p" comment print_enum_member member) in
     Pretty.horiz_vertic pp pp
 ;
 
@@ -291,12 +332,26 @@ value options pc l =
   else pprintf pc "[%p]" (plist ~{sep=","} option_binding 2) l
 ;
 
+value pp_range pc = fun [
+  (n, None) -> pprintf pc "%d" n
+| (n, Some (NUM m)) -> pprintf pc "%d to %d" n m
+| (n, Some MAX) -> pprintf pc "%d to max" n
+]
+;
+value pp_ident pc id = pprintf pc "%s" id ;
+
 EXTEND_PRINTER
   pr_oneof_member:
     [ [ OM_EMPTY _ -> pprintf pc ";"
       | OM_OPTION _ (n, c) -> pprintf pc "option %p;" option_binding (n,c)
       | OM_FIELD _ f -> pprintf pc "%p %s = %d %p;"
           typ f.of_type f.of_name f.of_num options f.of_options
+    ] ] ;
+  pr_enum_member:
+    [ [ EM_EMPTY _ -> pprintf pc ";"
+      | EM_OPTION _ (n, c) -> pprintf pc "option %p;" option_binding (n,c)
+      | EM_FIELD _ n num opts -> pprintf pc "%s = %d %p;"
+          n num options opts
     ] ] ;
   pr_member:
     [ [ MM_FIELD _ f ->
@@ -315,12 +370,9 @@ EXTEND_PRINTER
       | MM_MAP _ keyty valty n num opts ->
         pprintf pc "map<%p,%p>%s = %d%p;" typ keyty typ valty n num options opts
       | MM_EXTENSIONS _ l ->
-        let pp_range pc = fun [
-          (n, None) -> pprintf pc "%d" n
-       | (n, Some (NUM m)) -> pprintf pc "%d to %d" n m
-       | (n, Some MAX) -> pprintf pc "%d to max" n
-       ] in
-        pprintf pc "extensions %p;" (plist ~{sep=","} pp_range 2) l
+         pprintf pc "extensions %p;" (plist ~{sep=","} pp_range 2) l
+       | MM_RESERVED _ (Left l) -> pprintf pc "reserved %p;" (plist ~{sep=","} pp_range 2) l
+       | MM_RESERVED _ (Right l) -> pprintf pc "reserved %p;" (plist ~{sep=","} pp_ident 2) l
     ] ] ;
   pr_stmt:
     [ [ EMPTY _ -> pprintf pc ";"
@@ -332,6 +384,8 @@ EXTEND_PRINTER
       | OPTION _ n c -> pprintf pc "option %p;" option_binding (n,c)
       | MESSAGE _ n l -> pprintf pc "message %s @[<2>{@ %p@ }@]"
           n (plist print_commented_member 2) l
+      | ENUM _ n l -> pprintf pc "enum %s @[<2>{@ %p@ }@]"
+          n (plist print_commented_enum_member 2) l
       ]
     ]
   ;
