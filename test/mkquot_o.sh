d2f6537bce8eda1084d2e80d790a0e81477ddf48
Chet Murthy <chetsky@gmail.com>
2020-08-13 15:03:14 -0700

    rehab more of "test" dir
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch master
    # Your branch is ahead of 'origin/master' by 6 commits.
    #   (use "git push" to publish your local commits)
    #
    # Changes to be committed:
    #	modified:   test/Makefile
    #	modified:   test/mkquot_o.sh
    #	modified:   test/quot_o.ml
    #
    # Untracked files:
    #	testsuite/foo.ml
    #	tutorials/streams/include_ml
    #

--- test/mkquot_o.sh
+++ test/mkquot_o.sh
@@ -2,5 +2,9 @@
 # mkquot_o.sh,v
 
 head -n2 quot_o.ml
-../meta/camlp5r -nolib -I ../meta ../etc/pa_mktest.cmo ../etc/pr_o.cmo -flag M -impl ../main/mLast.mli |
-sed -e '1,/begin_stuff/d; /end_stuff/,$d'
+../meta/camlp5r -nolib -I ../meta ../etc/pa_mktest.cmo ../meta/pa_macro.cmo ../etc/pr_o.cmo \
+	-pa_mktest-ignore-type class_infos \
+	-pa_mktest-expand-type generic_constructor \
+	-pa_mktest-expand-type extension_constructor \
+	-pa_mktest-expand-type type_extension \
+	-l 400 -flag M -sep '\n' -impl ../main/mLast.mli
