69f2bea4f6e6fe0acf9639b3a1cb80e6831707dc
Chet Murthy <chetsky@gmail.com>
2020-08-14 11:15:07 -0700

    saving changes to test/mkquot_o.sh -- still doesn't work
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch master
    # Your branch is up to date with 'origin/master'.
    #
    # Changes to be committed:
    #	modified:   test/mkquot_o.sh
    #	modified:   test/quot_o.ml
    #
    # Changes not staged for commit:
    #	modified:   testsuite/include_ml
    #
    # Untracked files:
    #	testsuite/foo.ml
    #	tutorials/streams/include_ml
    #

--- test/mkquot_o.sh
+++ test/mkquot_o.sh
@@ -4,6 +4,12 @@
 head -n2 quot_o.ml
 ../meta/camlp5r -nolib -I ../meta ../etc/pa_mktest.cmo ../meta/pa_macro.cmo ../etc/pr_o.cmo \
 	-pa_mktest-ignore-type class_infos \
+	-pa_mktest-ignore-type case_branch \
+	-pa_mktest-ignore-type generic_constructor \
+	-pa_mktest-ignore-type longid_lident \
+	-pa_mktest-ignore-type payload \
+	-pa_mktest-expand-type attributes \
+	-pa_mktest-expand-type payload \
 	-pa_mktest-expand-type generic_constructor \
 	-pa_mktest-expand-type extension_constructor \
 	-pa_mktest-expand-type type_extension \
