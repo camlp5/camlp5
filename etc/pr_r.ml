3474e961ce17aa7917ce5e5fce03c03c97a8df2d
Chet Murthy <chetsky@gmail.com>
2024-04-24 13:26:49 -0700

    more baby-steps for raw identifiers

--- etc/pr_r.ml
+++ etc/pr_r.ml
@@ -74,8 +74,8 @@ value is_infix = do {
 };
 
 value is_keyword =
-  let keywords = ["declare"; "value"; "where"] in
-  fun s -> List.mem s keywords
+  let kwdhash = R_keywords.keywords_hash in
+  fun s -> Hashtbl.mem kwdhash s
 ;
 
 value has_special_chars s =
@@ -177,10 +177,17 @@ value expand_lprintf pc loc f =
   else f pc
 ;
 
+
+value string pc s = pprintf pc "\"%s\"" s;
+
+value lident pc v =
+  if is_keyword v then pprintf pc "\\#%s@ " v
+  else pprintf pc "%s" v
+;
+
 value var_escaped pc (loc, v) =
   if is_infix v || has_special_chars v then lprintf pc "\\%s@ " v
-  else if is_keyword v then lprintf pc "\\%s@ " v
-  else lprintf pc "%s" v
+  else lident pc v
 ;
 
 value cons_escaped pc s =
@@ -276,7 +283,7 @@ value pr_extension atstring pc attr =
 value longident_lident pc (lio, id) =
   match lio with
   [ None -> pprintf pc "%s" (Pcaml.unvala id)
-  | Some li -> pprintf pc "%p.%s" longident (Pcaml.unvala li) (Pcaml.unvala id)
+  | Some li -> pprintf pc "%p.%p" longident (Pcaml.unvala li) lident (Pcaml.unvala id)
   ]
 ;
 
@@ -1156,9 +1163,6 @@ value expr_short pc x =
 ;
 
 (* definitions of printers *)
-
-value string pc s = pprintf pc "\"%s\"" s;
-value lident pc s = pprintf pc "%s" s;
 
 value external_decl pc (loc, n, tyvars, t, sl, attrs) =
   pprintf pc "external %p :@;%p%p = %s%p" var_escaped (loc, n) typevars_binder tyvars ctyp t
