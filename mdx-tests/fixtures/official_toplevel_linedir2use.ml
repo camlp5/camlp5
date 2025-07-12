921486c08386deed282e03680487ecda0fd76c0a
Chet Murthy <chetsky@gmail.com>
2025-07-11 20:45:37 -0700

    move mdx-tests to top dir

---
+++ mdx-tests/fixtures/official_toplevel_linedir2use.ml
@@ -0,0 +1,5 @@
+#use "../local-install/lib/ocaml/topfind.camlp5";;
+#require "fmt";;
+#camlp5o ;;
+# 10 "fixtures/r.ml"
+#use "use1.ml" ;;
--- testsuite/mdx-tests/fixtures/official_toplevel_linedir2.ml
+++
@@ -1,4 +0,0 @@
-#use "../../local-install/lib/ocaml/topfind.camlp5";;
-#camlp5o ;;
-# 10 "fixtures/r.ml"
-let _ _ = 1 ;;
