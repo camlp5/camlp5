a7a925a77034d78895da98605490edf3d4f0afc5
Chet Murthy <chetsky@gmail.com>
2026-05-15 14:22:02 -0700

    meh

---
+++ tools/depend2.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+
+use strict ;
+
+our $verbose ;
+
+while (@ARGV) {
+  if ($ARGV[0] eq '-v') {
+    shift @ARGV ;
+    $verbose = 1 ;
+  }
+  else { last ; }
+}
+
+{
+  my $launch = "env TOP=.. ocamlfind camlp5-buildscripts/LAUNCH --" ;
+
+  foreach my $f (@ARGV) {
+    my $l = `head -1 $f` ;
+    if ($l =~ m/camlp5r/) {
+      v_system("${launch} ocamldep -pp 'camlp5r -I ../local-install/lib/camlp5' $f")
+    }
+    else {
+      v_system("${launch} ocamldep -pp 'camlp5o -I ../local-install/lib/camlp5' $f")
+    }
+  }
+}
+
+sub v_system {
+  my $cmd = shift;
+  print STDERR "<<$cmd>>\n" if ($main::verbose);
+  system $cmd;
+}
