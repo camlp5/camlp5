cba1ab447fcc6ae8402468818f92e47d7aec372e
Chet Murthy <chetsky@gmail.com>
2026-06-04 12:40:22 -0700

    pattern matrix-test compiles, elicits errors

--- testsuite/o2patr_matrixtest.ml
+++ testsuite/o2patr_matrixtest.ml
@@ -2,6 +2,8 @@
 (* camlp5r *)
 (* o2r_test.ml *)
 
+[@@@warnerror "-generative-application-expects-unit";];
+
 open Testutil;
 open Testutil2;
 
@@ -12,14 +14,14 @@ Pcaml.inter_phrases.val := Some ";\n" ;
 
 
 module PatRP = struct
-  module Base = PrintBase(struct end) ;
+  module Base = Mlsyntax.PrintBase(struct end) ;
   module R = Print_patr.PP(Base) ;
   module RO = Print_patro.PP(Base)(R) ;
   module RP = Print_patrp.PP(Base)(R) ;
-  module Pretty = PrettyPrint(Base.Printers) ;
+  module Pretty = MLPrinters.PrettyPrint(Base.Printers) ;
 end ;
 
-module PAPR = PAPRGen(MLParsers.OP.Base)(MLPrinters.RP.Base.Printers) ;
+module PAPR = PAPRGen(MLParsers.OP.Base)(PatRP.Base.Printers) ;
 
 value tests = "matrix" >::: (Papr_test_matrix.o2r PAPR.both_pa1 PAPR.both_pr None ()) ;
 
